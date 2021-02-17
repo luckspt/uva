48f6ecc611ddfbbbbf5040f75376aac589603bc4
Lucas Pinto <lucks@outlook.pt>
2021-02-17 22:49:04 +0000

    483

--- 483-Word Scramble/483.py
+++ 483-Word Scramble/483.py
@@ -1,3 +1,5 @@
-# Runtime Error
-while True:
-    print(' '.join([word[::-1] for word in input().split(' ')]))
+# Accepted
+import sys
+for line in sys.stdin:
+    line = line.strip('\n')
+    print(' '.join([word[::-1] for word in line.split(' ')]))
